a2a813a87a273a79333fd054a833d514e0c89fb7
Shay Davidson <shay.davidson@lemonade.com>
2019-05-17 23:01:42 +0300

    Updates

---
+++ precache-manifest.b585307dcf1baf829ee88e97182b2946.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "920fcdc0687ee426f5bbb6c789ddca9a",
+    "url": "/create-galaxy/index.html"
+  },
+  {
+    "revision": "88a670c1ec1504e8c1c0",
+    "url": "/create-galaxy/static/css/main.0af64d1e.chunk.css"
+  },
+  {
+    "revision": "9bc38fadb9db664819fc",
+    "url": "/create-galaxy/static/js/2.9c37452f.chunk.js"
+  },
+  {
+    "revision": "88a670c1ec1504e8c1c0",
+    "url": "/create-galaxy/static/js/main.5bf553f0.chunk.js"
+  },
+  {
+    "revision": "0d7df327b2564bc60a16",
+    "url": "/create-galaxy/static/js/runtime~main.9bab7769.js"
+  }
+]);
